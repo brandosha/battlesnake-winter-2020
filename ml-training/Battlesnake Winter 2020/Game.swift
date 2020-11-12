f8d55da00b899b937fc8a11ec49b2442fcfd1386
Brandon Lantz <brandon.lantz@icloud.com>
2020-11-11 22:58:01 -0700

    Implement basic program in swift

--- ml-training/Battlesnake Winter 2020/Game.swift
+++ ml-training/Battlesnake Winter 2020/Game.swift
@@ -12,8 +12,10 @@ class Game {
     struct Board {
         let width, height: Int
         
-        struct Positon {
-            let x, y: Int
+        static let standard = Board(width: 11, height: 11)
+        
+        struct Positon: Hashable {
+            var x, y: Int
             
             static func random(in board: Board) -> Positon {
                 return Positon(
@@ -22,27 +24,207 @@ class Game {
                 )
             }
         }
+        
+        enum Direction {
+            case up, down, left, right
+            
+            static func random() -> Direction {
+                let all: [Direction] = [.up, .down, .left, .right]
+                return all.randomElement()!
+            }
+            
+            func appliedTo(_ position: Positon) -> Positon {
+                var xVal = position.x
+                var yVal = position.y
+                
+                switch self {
+                case .up:
+                    yVal += 1
+                case .down:
+                    yVal -= 1
+                case .left:
+                    xVal -= 1
+                case .right:
+                    xVal += 1
+                }
+                
+                return Positon(x: xVal, y: yVal)
+            }
+        }
     }
     
     class Snake {
-        let head: Board.Positon
-        let body: [Board.Positon]
+        var facing: Board.Direction = .random()
+        var head: Board.Positon
+        var body: [Board.Positon]
+        var isAlive: Bool = true
+        var id: String = {
+            let chars = "0123456789abcdefghijklmnopqrstuvwxyz"
+            return String((0...16).map { _ in chars.randomElement()! })
+        }()
+        
+        let bodyString = String("HXZN".randomElement()!)
+        let headString = String("@0¢".randomElement()!)
         
         init(
-            board: Board = Board(width: 11, height: 11),
+            board: Board = .standard,
             head: Board.Positon? = nil,
             length: Int = 3
         ) {
             self.head = head ?? .random(in: board)
             self.body = .init(repeating: self.head, count: 3)
         }
+        
+        func move(_ direction: Board.Direction) {
+            self.head = direction.appliedTo(self.head)
+            
+            self.body.insert(self.head, at: 0)
+            self.body.removeLast()
+        }
     }
     
     let board: Board
     let snakes: [Snake]
+    var food: Set<Board.Positon> = []
+    var remainingSnakes: Int
     
-    init(_ board: Board, snakes: [Snake]) {
+    init(_ board: Board = .standard, snakes: [Snake]) {
         self.board = board
         self.snakes = snakes
+        self.remainingSnakes = snakes.count
     }
+    
+    func doMoves(selectMove: (Snake, Game) -> Board.Direction) {
+        for snake in snakes {
+            guard snake.isAlive else { continue }
+            
+            let move = selectMove(snake, self)
+            snake.move(move)
+            
+            if (
+                !(0...self.board.width - 1 ~= snake.head.x) ||
+                !(0...self.board.height - 1 ~= snake.head.y)
+            ) {
+                snake.isAlive = false
+                remainingSnakes -= 1
+            } else if food.contains(snake.head) {
+                food.remove(snake.head)
+                snake.body.append(snake.body.last!)
+            }
+        }
+        
+        var snakeBodies: Dictionary<Board.Positon, (snake: Snake, isHead: Bool)> = [:]
+        for snake in snakes {
+            guard snake.isAlive else { continue }
+            
+            for (index, bodySegment) in snake.body.enumerated() {
+                let isHead = index == 0
+                if let collidedWith = snakeBodies[bodySegment] {
+                    if collidedWith.isHead {
+                        collidedWith.snake.isAlive = false
+                        remainingSnakes -= 1
+                    }
+                    if isHead {
+                        snake.isAlive = false
+                        remainingSnakes -= 1
+                    }
+                } else {
+                    snakeBodies[bodySegment] = (snake, isHead)
+                }
+            }
+        }
+        // cachedSnakeBodies = snakeBodies
+        
+        boardDict = getBoardDict()
+    }
+    
+    /*private lazy var cachedSnakeBodies: Dictionary<Board.Positon, (snake: Snake, isHead: Bool)> = {
+        var snakeBodies: Dictionary<Board.Positon, (snake: Snake, isHead: Bool)> = [:]
+        for snake in snakes {
+            guard snake.isAlive else { continue }
+            
+            for (index, bodySegment) in snake.body.enumerated() {
+                let isHead = index == 0
+                if snakeBodies[bodySegment] != nil && isHead {
+                    snakeBodies[bodySegment] = (snake, isHead)
+                } else {
+                    snakeBodies[bodySegment] = (snake, isHead)
+                }
+            }
+        }
+        
+        return snakeBodies
+    }()*/
+    
+    enum Entity {
+        case head(Snake)
+        case body(Snake)
+        case food
+    }
+    
+    lazy var boardDict: Dictionary<Board.Positon, Entity?> = {
+        return getBoardDict()
+    }()
+    
+    private func getBoardDict() -> Dictionary<Board.Positon, Entity?> {
+        var boardDict: Dictionary<Board.Positon, Entity?> = [:]
+        
+        for foodPos in food {
+            boardDict[foodPos] = .food
+        }
+        
+        for snake in snakes {
+            for bodySegment in snake.body {
+                if bodySegment == snake.head {
+                    boardDict[bodySegment] = .head(snake)
+                } else {
+                    boardDict[bodySegment] = .body(snake)
+                }
+            }
+        }
+        
+        return boardDict
+    }
+    
+    func printBoard() {
+        var textRows: [String] = [.init(repeating: "-", count: board.width + 2)]
+        for row in 0...board.height - 1 {
+            var textRow: String = "|"
+            for col in 0...board.width - 1 {
+                let pos = Board.Positon(x: col, y: row)
+                
+                switch boardDict[pos] {
+                case .body(let snake):
+                    if snake.isAlive { textRow += snake.bodyString }
+                    else { textRow += " " }
+                case .head(let snake):
+                    if snake.isAlive { textRow += snake.headString }
+                    else { textRow += " " }
+                case .food:
+                    textRow += "*"
+                default:
+                    textRow += " "
+                }
+                
+                /*if let bodyPart = cachedSnakeBodies[pos] {
+                    if (bodyPart.isHead) {
+                        textRow += "@"
+                    } else {
+                        textRow += "#"
+                    }
+                } else if food.contains(pos) {
+                    textRow += "*"
+                } else {
+                    textRow += " "
+                }*/
+            }
+            
+            textRow += "|"
+            textRows.append(textRow)
+        }
+        textRows.append(.init(repeating: "-", count: board.width + 2))
+        
+        print(textRows.joined(separator: "\n"))
+    }
+    
 }
--- ml-training/Battlesnake Winter 2020/Train.swift
+++ ml-training/Battlesnake Winter 2020/Train.swift
@@ -11,5 +11,43 @@ import ArgumentParser
 struct Train: ParsableCommand {
     func run() throws {
         print("Training...")
+        
+        let game = Game(snakes: [Game.Snake(), Game.Snake()])
+        
+        var stepsPlayed = 0
+        
+        game.printBoard()
+        while game.remainingSnakes > 1 {
+            if (Bool.random()) {
+                game.food.insert(.random(in: game.board))
+            }
+            game.doMoves { snake, game in
+                let allMoves: [Game.Board.Direction] = [.up, .down, .left, .right]
+                
+                let okMoves = allMoves.filter { move in
+                    let newPos = move.appliedTo(snake.head)
+                    
+                    if !(0...game.board.width - 1 ~= newPos.x) { return false }
+                    if !(0...game.board.height - 1 ~= newPos.y) { return false }
+                    
+                    guard let boardEntity = game.boardDict[newPos] else { return true }
+                    
+                    switch boardEntity {
+                    case .food:
+                        return true
+                    default:
+                        return false
+                    }
+                }
+                
+                return okMoves.randomElement() ?? .random()
+            }
+            usleep(100 * 1000)
+            game.printBoard()
+            
+            stepsPlayed += 1
+        }
+        
+        print("Steps played: \(stepsPlayed)")
     }
 }
